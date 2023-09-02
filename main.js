d4c8c544457afbd11430f583b7ec312d7301773e
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-09-02 22:00:19 +0900

    Add correlation coefficients sum

--- main.js
+++ main.js
@@ -9,42 +9,223 @@ const switch3 = document.getElementById("switch3");
 const radarCanvas = document.getElementById("radarCanvas");
 const scatterCanvas = document.getElementById("scatterCanvas");
 
-// Chart variables
-let radarChart; // keeping reference to radar chart
-let scatterChart; // keeping referene to scatter chart
-let scatterData = [];
-let scatterLabels = [];
-let radarData = []; // keeping correlation data for specific key
-let radarLabels = []; // keeping labels. I want to remove IQ to IQ thus need to keep array filed in the same order as data
-let keys = []; // keeping all the object keys names ["IQ","Fertility",etc] for radar
-let correlationCountries = 0; // number of countries based on which the correlation was calculated
-let totalPopulation = 0; // total globe population
+// variables
 let option1 = switch1.value;
 let option2 = switch2.value;
 let option3 = switch3.value;
+let radarChart; // keeping reference to radar chart
+let radarData = []; // keeping correlation data for specific key
+let radarLabels = []; // keeping labels. I want to remove IQ to IQ thus need to keep array filed in the same order as data
+let scatterChart; // keeping referene to scatter chart
+let scatterData = [];
+let scatterLabels = [];
+let correlationCountries = 0; // number of countries based on which the correlation was calculated
+let totalPopulation = 0; // total globe population
 
-// Event listeners
+// event listeners
 switch1.addEventListener("change", handlerSwitch1Change);
 switch2.addEventListener("change", showCorrelationResult);
 switch3.addEventListener("change", handlerSwitch3Change);
 
-// fill the radarData with all the correlations coefficients of given @mainKey
-function collectAllCoefficient(mainKey, data) {
-    radarData = [];
-    radarLabels = [];
+// return all non empty values of key @key1 but only if @key2 existing there too
+// make the mean returned weighted by the population
+function extractKeyValues(key1, key2, jsonData) {
+    const results = {
+        x: [],
+        y: [],
+        countries: [],
+        xWeightedMean: 0,
+        yWeightedMean: 0,
+    };
+    const jsonDataValues = Object.values(jsonData);
+    let xWeightedSum = 0;
+    let yWeightedSum = 0;
+    let relatedPopulation = 0;
 
-    Object.keys(data[0]).forEach((secondKey) => {
-        if (secondKey !== "Country" && mainKey !== secondKey) {
-            const correlationValue = Math.abs(
-                correlationCoefficient(mainKey, secondKey, data)
-            ).toFixed(2);
-            radarData.push(correlationValue);
-            radarLabels.push(secondKey);
+    jsonDataValues.forEach((obj) => {
+        const value1 = obj[key1];
+        const value2 = obj[key2];
+        const population = obj["Population"];
+
+        if (
+            value2 !== undefined &&
+            value2 !== null &&
+            value1 !== undefined &&
+            value1 !== null &&
+            population
+        ) {
+            results.x.push(value1);
+            results.y.push(value2);
+            results.countries.push(obj["Country"]);
+
+            xWeightedSum += value1 * population;
+            yWeightedSum += value2 * population;
+            relatedPopulation += population;
+        }
+    });
+
+    results.xWeightedMean = xWeightedSum / relatedPopulation;
+    results.yWeightedMean = yWeightedSum / relatedPopulation;
+
+    return results;
+}
+// calculating weighted mean based on number of people in the country.
+function showMean(key, jsonData) {
+    const jsonDataValues = Object.values(jsonData);
+    const abbr = document.getElementById(`${key}Abbr`);
+    const bold = document.getElementById(key);
+
+    let values = 0;
+    let countriesCount = 0;
+    let relatedPopulation = 0;
+
+    for (let i = 0; i < jsonDataValues.length; i++) {
+        const obj = jsonDataValues[i];
+        const value = obj[key];
+        if (value !== null) {
+            if (!obj["Population"])
+                console.error(
+                    "For some reasons population is empty. This should not happen as all the countries should have this value populated!"
+                );
+            relatedPopulation += obj["Population"];
+            values += value * obj["Population"];
+            countriesCount++;
+        }
+    }
+
+    const mean = (values / relatedPopulation).toFixed(2);
+
+    if (abbr) {
+        abbr.setAttribute(
+            "data-title",
+            abbr.getAttribute("data-title") +
+                ` Based on data from ${countriesCount} countries`
+        );
+    }
+
+    if (key === "BraSize") {
+        if (mean < 3.5) bold.innerText = "C";
+        if (mean < 3) bold.innerText = "B-C";
+        if (mean < 2.5) bold.innerText = "B";
+        if (mean < 2) bold.innerText = "A-B";
+        if (mean < 1.5) bold.innerText = "A";
+        if (mean < 1) bold.innerText = "AA-A";
+        if (mean < 0.5) bold.innerText = "AA";
+    } else {
+        bold.innerText = mean;
+    }
+}
+// Get the strength of correlation based on value
+function getCorrelationStrength(value) {
+    let result = "";
+    if (value <= 1) result = "very strong positive";
+    if (value < 0.9) result = "strong positive";
+    if (value < 0.7) result = "moderate positive";
+    if (value < 0.4) result = "weak positive";
+    if (value < 0.2) result = "no";
+    if (value <= -0.2) result = "weak negative";
+    if (value <= -0.4) result = "moderate negative";
+    if (value <= -0.7) result = "strong negative";
+    if (value <= -0.9) result = "very strong negative";
+
+    return result;
+}
+// show data for given country
+function showCountryStats(country, jsonData) {
+    const countryStats = Object.values(jsonData).filter(
+        (obj) => obj["Country"] === country
+    )[0];
+
+    Object.keys(countryStats).forEach((key) => {
+        if (key !== "Country") {
+            const bold = document.getElementById(key);
+            const value = countryStats[key];
+
+            if (key === "BraSize") {
+                if (value < 3.5) bold.innerText = "C";
+                if (value < 3) bold.innerText = "B-C";
+                if (value < 2.5) bold.innerText = "B";
+                if (value < 2) bold.innerText = "A-B";
+                if (value < 1.5) bold.innerText = "A";
+                if (value < 1) bold.innerText = "A-AA";
+                if (value < 0.5) bold.innerText = "AA";
+            } else {
+                bold.innerText = value;
+            }
         }
     });
 }
+// calculate the correlation coefficient
+function correlationCoefficient(key1, key2, jsonData) {
+    // gather all the infomation before calculation and place them on the scatter chart
+    const extractedValues = extractKeyValues(key1, key2, jsonData);
+    const x = extractedValues.x;
+    const y = extractedValues.y;
 
-// Show correlation result based on selected options
+    if (x.length !== y.length)
+        console.error(
+            "The x and y values have different lenght. This should never happen!"
+        );
+
+    const xMean = extractedValues.xWeightedMean;
+    const yMean = extractedValues.yWeightedMean;
+    const countries = extractedValues.countries;
+
+    correlationCountries = countries.length;
+    scatterLabels = [key1, key2, countries];
+    scatterData = []; // empty the scatterData so the chart doesn't accumulate data
+
+    for (let index = 0; index < x.length; index++) {
+        // first key to be on the y and the second on the x scale
+        const location = { x: y[index], y: x[index] };
+        scatterData.push(location);
+    }
+
+    // the main calculation
+    let numerator = 0;
+    let xSquaredSum = 0;
+    let ySquaredSum = 0;
+
+    for (let i = 0; i < x.length; i++) {
+        numerator += (x[i] - xMean) * (y[i] - yMean);
+        xSquaredSum += Math.pow(x[i] - xMean, 2);
+        ySquaredSum += Math.pow(y[i] - yMean, 2);
+    }
+
+    const denominator = Math.sqrt(xSquaredSum) * Math.sqrt(ySquaredSum);
+    const correlation = numerator / denominator;
+
+    return correlation;
+}
+// collect all the nesesary data for radar chart of key @mainkey
+function collectAllCoefficient(mainKey, jsonData) {
+    const totalResult = document.getElementById("totalResult");
+    const totalAbbr = document.getElementById("totalAbbr");
+
+    radarData = [];
+    radarLabels = [];
+    let sum = 0;
+    let maxValue = 0;
+
+    Object.keys(jsonData[0]).forEach((secondKey) => {
+        if (secondKey !== "Country" && mainKey !== secondKey) {
+            const correlationValue = Math.abs(
+                correlationCoefficient(mainKey, secondKey, jsonData)
+            );
+            sum += correlationValue;
+            maxValue++;
+            radarData.push(correlationValue.toFixed(2));
+            radarLabels.push(secondKey);
+        }
+    });
+
+    totalResult.innerText = sum.toFixed(2);
+    totalAbbr.setAttribute(
+        "data-title",
+        `0 - the weakest, ${maxValue} - the strongest`
+    );
+}
+// show correlation result based on selected options
 function showCorrelationResult() {
     option2 = switch2.value;
 
@@ -58,47 +239,6 @@ function showCorrelationResult() {
 
     scatterChart && updateScatterChart();
 }
-
-// Get the strength of correlation based on value
-function getCorrelationStrength(value) {
-    const thresholds = [0.2, 0.4, 0.7, 0.9];
-    const strengths = ["no", "weak", "moderate", "strong", "very strong"];
-
-    let absValue = Math.abs(value);
-    let strengthIndex = 0;
-
-    for (let i = 0; i < thresholds.length; i++) {
-        if (absValue >= thresholds[i]) {
-            strengthIndex = i + 1;
-        }
-    }
-
-    if (strengthIndex === 0) return "no";
-
-    if (value < 0) {
-        return `${strengths[strengthIndex]} negative`;
-    } else {
-        return `${strengths[strengthIndex]} positive`;
-    }
-}
-
-function updateRadarChart() {
-    radarChart.data.labels = radarLabels;
-    radarChart.data.datasets[0].data = radarData;
-    radarChart.options.plugins.title.text = `${option1} correlations radar chart`;
-    radarChart.update();
-}
-
-function updateScatterChart() {
-    scatterChart.data.datasets[0].data = scatterData;
-    scatterChart.data.labels = scatterLabels[2];
-    scatterChart.options.scales.x.title.text = scatterLabels[1];
-    scatterChart.options.scales.y.title.text = scatterLabels[0];
-    scatterChart.options.plugins.title.text = `Based on data from ${correlationCountries} countries`;
-
-    scatterChart.update();
-}
-
 function showRadarChart() {
     radarChart = new Chart(radarCanvas, {
         type: "radar",
@@ -123,6 +263,12 @@ function showRadarChart() {
             plugins: {
                 legend: {
                     display: false,
+                },
+            },
+            scales: {
+                r: {
+                    suggestedMin: 0,
+                    suggestedMax: 1,
                 },
             },
             elements: {
@@ -212,6 +358,21 @@ function showScatterChart() {
         },
     });
 }
+function updateRadarChart() {
+    radarChart.data.labels = radarLabels;
+    radarChart.data.datasets[0].data = radarData;
+    radarChart.options.plugins.title.text = `${option1} correlations radar chart`;
+    radarChart.update();
+}
+function updateScatterChart() {
+    scatterChart.data.datasets[0].data = scatterData;
+    scatterChart.data.labels = scatterLabels[2];
+    scatterChart.options.scales.x.title.text = scatterLabels[1];
+    scatterChart.options.scales.y.title.text = scatterLabels[0];
+    scatterChart.options.plugins.title.text = `Based on data from ${correlationCountries} countries`;
+
+    scatterChart.update();
+}
 // handling change of the first selector
 function handlerSwitch1Change() {
     // Get the selected value from Switch 1
@@ -236,198 +397,6 @@ function handlerSwitch3Change() {
     // update radar title
     radarChart && updateRadarChart();
 }
-// calculating weighted valuebased on number of people in the country.
-// Total Average = (Average A * Population A + Average B * Population B) / (Population A + Population B)
-// So add total population and collect info about countries and multiply it by population then
-function showMean(key, jsonData) {
-    let values = 0;
-    let countriesCount = 0;
-    let relatedPopulation = 0;
-    const jsonDataValues = Object.values(jsonData);
-
-    for (let i = 0; i < jsonDataValues.length; i++) {
-        const obj = jsonDataValues[i];
-        const value = obj[key];
-        if (value !== null) {
-            if (!obj["Population"])
-                console.error(
-                    "For some reasons population is empty. This should not happen as all the countries should have this value populated!"
-                );
-            relatedPopulation += obj["Population"];
-            values += value * obj["Population"];
-            countriesCount++;
-        }
-    }
-
-    const bold = document.getElementById(key);
-    const mean = (values / relatedPopulation).toFixed(2);
-    const abbr = document.getElementById(`${key}Abbr`);
-
-    if (abbr) {
-        abbr.setAttribute(
-            "data-title",
-            abbr.getAttribute("data-title") +
-                ` Based on data from ${countriesCount} countries`
-        );
-    }
-
-    if (key === "BraSize") {
-        if (mean < 3.5) bold.innerText = "C";
-        if (mean < 3) bold.innerText = "B-C";
-        if (mean < 2.5) bold.innerText = "B";
-        if (mean < 2) bold.innerText = "A-B";
-        if (mean < 1.5) bold.innerText = "A";
-        if (mean < 1) bold.innerText = "AA-A";
-        if (mean < 0.5) bold.innerText = "AA";
-    } else {
-        bold.innerText = mean;
-    }
-}
-function showCountryStats(country, jsonData) {
-    const countryStats = Object.values(jsonData).filter(
-        (obj) => obj["Country"] === country
-    )[0];
-
-    Object.keys(countryStats).forEach((key) => {
-        if (key !== "Country") {
-            const bold = document.getElementById(key);
-            const value = countryStats[key];
-
-            if (key === "BraSize") {
-                if (value < 3.5) bold.innerText = "C";
-                if (value < 3) bold.innerText = "B-C";
-                if (value < 2.5) bold.innerText = "B";
-                if (value < 2) bold.innerText = "A-B";
-                if (value < 1.5) bold.innerText = "A";
-                if (value < 1) bold.innerText = "A-AA";
-                if (value < 0.5) bold.innerText = "AA";
-            } else {
-                bold.innerText = value;
-            }
-        }
-    });
-}
-// return all non empty values of key @key1 but only if @key2 existing there too
-// make the mean returned weighted by the population
-function extractKeyValues(key1, key2, jsonData) {
-    const results = {
-        x: [],
-        y: [],
-        countries: [],
-        xWeightedMean: 0,
-        yWeightedMean: 0,
-    };
-    const jsonDataValues = Object.values(jsonData);
-    let xWeightedSum = 0;
-    let yWeightedSum = 0;
-    let relatedPopulation = 0;
-
-    jsonDataValues.forEach((obj) => {
-        const value1 = obj[key1];
-        const value2 = obj[key2];
-        const population = obj["Population"];
-
-        if (
-            value2 !== undefined &&
-            value2 !== null &&
-            value1 !== undefined &&
-            value1 !== null &&
-            population
-        ) {
-            results.x.push(value1);
-            results.y.push(value2);
-            results.countries.push(obj["Country"]);
-
-            xWeightedSum += value1 * population;
-            yWeightedSum += value2 * population;
-            relatedPopulation += population;
-        }
-    });
-
-    results.xWeightedMean = xWeightedSum / relatedPopulation;
-    results.yWeightedMean = yWeightedSum / relatedPopulation;
-
-    return results;
-}
-function extractKeyCountries(key1, key2, jsonData) {
-    const result = [];
-    for (const countryData of jsonData) {
-        if (key1 in countryData && key2 in countryData) {
-            if (countryData[key1] !== null && countryData[key2] !== null) {
-                result.push(countryData["Country"]);
-            }
-        }
-    }
-    return result;
-}
-function calculateMean(array) {
-    // TODO: function calculateMean(array,population) {
-    if (array.length === 0) {
-        return 0; // Return 0 for an empty array, or you could choose to return NaN or throw an error.
-    }
-
-    const sum = array.reduce(
-        (accumulator, currentValue) => accumulator + currentValue,
-        // TODO: (accumulator, currentValue) => accumulator * population + currentValue,
-        0
-    );
-    const mean = sum / array.length;
-
-    return mean;
-}
-
-// Calculate the correlation coefficient
-function correlationCoefficient(key1, key2, jsonData) {
-    const extractedValues = extractKeyValues(key1, key2, jsonData);
-
-    const x = extractedValues.x;
-    const y = extractedValues.y;
-    const xMean = extractedValues.xWeightedMean;
-    const yMean = extractedValues.yWeightedMean;
-    const countries = extractedValues.countries;
-
-    scatterLabels = [key1, key2, countries];
-    correlationCountries = countries.length;
-
-    if (x.length !== y.length)
-        console.error(
-            "The x and y values have different lenght. This should never happen!"
-        );
-
-    // Calculate the linear regression line
-    const sumX = x.reduce((sum, x) => sum + x, 0);
-    const sumY = y.reduce((sum, y) => sum + y, 0);
-    const sumXY = x.reduce((sum, x, i) => sum + x * y[i], 0);
-    const sumX2 = x.reduce((sum, x) => sum + x ** 2, 0);
-
-    const n = x.length;
-    slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX ** 2);
-    yIntercept = (sumY - slope * sumX) / n;
-
-    // empty the scatterData so the chart doesn't accumulate data
-    scatterData = [];
-    for (let index = 0; index < x.length; index++) {
-        // first key to be on the y and the second on the x scale
-        const location = { x: y[index], y: x[index] };
-        scatterData.push(location);
-    }
-
-    let numerator = 0;
-    let xSquaredSum = 0;
-    let ySquaredSum = 0;
-
-    for (let i = 0; i < x.length; i++) {
-        numerator += (x[i] - xMean) * (y[i] - yMean);
-        xSquaredSum += Math.pow(x[i] - xMean, 2);
-        ySquaredSum += Math.pow(y[i] - yMean, 2);
-    }
-
-    const denominator = Math.sqrt(xSquaredSum) * Math.sqrt(ySquaredSum);
-    const correlation = numerator / denominator;
-
-    return correlation;
-}
-
 // Initialize the application
 function initializeApp() {
     // iterate thru all the keys in objects
@@ -445,7 +414,6 @@ function initializeApp() {
     document.getElementById("Population").innerText = totalPopulation;
 
     // showCountryStats("Poland", data);
-
     handlerSwitch1Change();
     showScatterChart();
     showCorrelationResult();
